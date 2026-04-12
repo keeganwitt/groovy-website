939b01cac2b620585fe821830895d8180a610ff2
Paul King <paulk@asert.com.au>
2026-04-12 16:02:12 +1000

    add 5.0.5 and 4.0.31 Windows Installers

--- site/src/site/sitemap-dev.groovy
+++ site/src/site/sitemap-dev.groovy
@@ -89,12 +89,7 @@ downloads {
         }
         version('5.0.5') {
             stable true
-//            windowsInstaller 'https://groovy.jfrog.io/artifactory/dist-release-local/groovy-windows-installer/groovy-5.0.5/'
-        }
-        version('5.0.4') {
-            stable true
-            archive true
-            windowsInstaller 'https://groovy.jfrog.io/artifactory/dist-release-local/groovy-windows-installer/groovy-5.0.4/'
+            windowsInstaller 'https://groovy.jfrog.io/artifactory/dist-release-local/groovy-windows-installer/groovy-5.0.5/'
         }
     }
     distribution('Groovy 4.0') {
@@ -106,12 +101,7 @@ downloads {
         }
         version('4.0.31') {
             stable true
-//            windowsInstaller 'https://groovy.jfrog.io/artifactory/dist-release-local/groovy-windows-installer/groovy-4.0.31/'
-        }
-        version('4.0.30') {
-            stable true
-            archive true
-            windowsInstaller 'https://groovy.jfrog.io/artifactory/dist-release-local/groovy-windows-installer/groovy-4.0.30/'
+            windowsInstaller 'https://groovy.jfrog.io/artifactory/dist-release-local/groovy-windows-installer/groovy-4.0.31/'
         }
     }
     distribution('Groovy 3.0') {
