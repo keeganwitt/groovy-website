0a5ed2bb13e9530a8ef875b973384b553d9455b7
Paul King <paulk@asert.com.au>
2026-05-07 14:18:23 +1000

    Release 5.0.6: update sitemap

--- site/src/site/sitemap-dev.groovy
+++ site/src/site/sitemap-dev.groovy
@@ -87,8 +87,13 @@ downloads {
             a(href: 'versioning.html', 'version')
             yield ' of Groovy designed for JDK11+.'
         }
+        version('5.0.6') {
+            stable true
+//            windowsInstaller 'https://groovy.jfrog.io/artifactory/dist-release-local/groovy-windows-installer/groovy-5.0.6/'
+        }
         version('5.0.5') {
             stable true
+            archive true
             windowsInstaller 'https://groovy.jfrog.io/artifactory/dist-release-local/groovy-windows-installer/groovy-5.0.5/'
         }
     }
